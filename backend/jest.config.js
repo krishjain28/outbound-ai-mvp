b15b83efb2482c4a9b63f86da28311da42f519bd
krishjain28 <125991294+krishjain28@users.noreply.github.com>
2025-07-14 04:07:06 +0530

    Fix CI/CD pipeline: Disable conflicting workflows, add Jest config, fix test setup

---
+++ backend/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
+  collectCoverageFrom: [
+    '**/*.js',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!jest.config.js',
+    '!server.js'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  setupFilesAfterEnv: [],
+  testTimeout: 10000,
+  verbose: true
+}; 
